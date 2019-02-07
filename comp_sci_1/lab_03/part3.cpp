73677a99baeafabd86dd02e864944e871ad51a06
anease <46764285+anease@users.noreply.github.com>
2019-02-07 12:09:30 -0500

    Create part3.cpp

---
+++ comp_sci_1/lab_03/part3.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int width = 0;
+    int height = 0;
+
+    cout << "Width?";
+    cin >> width;
+    cout << "Height?";
+    cin >> height;
+    cout << endl;
+
+   for (int q = 0; q < height; q++ ) {
+
+   if (q == 0 || q == (height - 1)) {
+
+    for (int i = 0; i < width; i++) {
+        cout << "*";
+         }
+
+        }
+
+   else {
+
+       cout << "*";
+
+       for (int i = 1; i < (width - 1); i++ ) {
+           cout << " "; }
+
+       cout << "*";
+
+       }
+   cout << endl;
+
+   }
+
+
+    cout << endl;
+
+
+    return 0;
+}
